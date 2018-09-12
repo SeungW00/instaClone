6d7f646bc6a02673f346723649dae04c0ecb812b
SeungW00 <hsw0205@gmail.com>
2018-09-12 15:38:50 +0900

    following a User

--- instaclone/users/serializers.py
+++ instaclone/users/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from . import models
 from rest_framework import status
+
 class ExploreUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.User
         fields = (
+            'id',
             'profile_image',
             'username',
             'name'
