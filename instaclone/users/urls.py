693856e5904dce452f0d555ea9d07df65dd81147
SeungW00 <hsw0205@gmail.com>
2018-10-10 19:51:27 +0900

    update password

--- instaclone/users/urls.py
+++ instaclone/users/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path("<username>/following/", view=views.UserFollowing.as_view(), name="user_following"),
     path("search/", view=views.Search.as_view(), name="user_following"),
     path("<username>/", view=views.UserProfile.as_view(), name="user_profile"),
+    path("<username>/password/", view=views.ChangePassword.as_view(), name="change"),
     
 
     
