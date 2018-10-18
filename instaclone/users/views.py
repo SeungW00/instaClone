591cb5b40123f61e50d62e9b7752a64cf0fa9e26
SeungW00 <hsw0205@gmail.com>
2018-10-18 16:28:11 +0900

    login in with facebook

--- instaclone/users/views.py
+++ instaclone/users/views.py
@@ -3,7 +3,8 @@ from rest_framework.response import Response
 from . import models, serializers
 from rest_framework import status
 from instaclone.notifications import views as notification_views
-
+from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
+from rest_auth.registration.views import SocialLoginView
 
 class ExploreUsers(APIView):
 
@@ -214,6 +215,5 @@ class ChangePassword(APIView):
 
 
 
-
-    
-
+class FacebookLogin(SocialLoginView):
+    adapter_class = FacebookOAuth2Adapter
